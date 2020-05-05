5be49963f85fac863b4929962234bb0a85106a1b
Hieu Nguyen <minhhieun4@gmail.com>
2020-05-05 19:04:38 +0700

    fix

--- model/conv2d.py
+++ model/conv2d.py
@@ -84,7 +84,7 @@ class Conv2DSupervisor():
             Conv3D(filters=1,
                    kernel_size=(3, 3, 3),
                    padding='same',
-                   activation='relu'))
+                   activation='tanh'))
 
         print(model.summary())
 
