c941f5285f8ac327711185b8bd8d78b703eaba38
Hieu Nguyen <minhhieun4@gmail.com>
2020-05-06 16:38:05 +0700

    fix

--- model/utils/conv2d.py
+++ model/utils/conv2d.py
@@ -80,7 +80,8 @@ def create_data(**kwargs):
         target_conv2d_gsmap[i, :, :, :, 2] = _y
     
     target_conv2d_gsmap_2 = np.zeros(shape=(T, horizon, 72, 72, 3))
-    target_conv2d_gsmap_2 = target_conv2d_gsmap[:, :, 0:72, 0:72, :]
+    target_conv2d_gsmap_2 = target_conv2d_gsmap[:, :, 0:72, 0:72, 2]
+    target_conv2d_gsmap_2 = input_conv2d_gsmap[:, :, 0:72, 0:72, 0:2]
 
     return input_conv2d_gsmap, target_conv2d_gsmap_2
 
