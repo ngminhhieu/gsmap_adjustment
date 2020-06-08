739b3803665be0ae886e322c7fa722d540124571
Đinh Văn Hiếu <hieudv10@vingroup.net>
2020-06-08 20:51:28 +0700

    change filters to 16

--- model/conv2d.py
+++ model/conv2d.py
@@ -55,7 +55,7 @@ class Conv2DSupervisor():
         # model.add(MaxPooling3D(pool_size=(2, 2, 1)))
 
         model.add(
-            ConvLSTM2D(filters=32,
+            ConvLSTM2D(filters=16,
                        kernel_size=(5, 5),
                        padding='same',
                        activation=self.activation,
@@ -66,7 +66,7 @@ class Conv2DSupervisor():
         # model.add(MaxPooling3D(pool_size=(2, 2, 1)))
 
         model.add(
-            ConvLSTM2D(filters=32,
+            ConvLSTM2D(filters=16,
                        kernel_size=(5, 5),
                        padding='same',
                        activation=self.activation,
