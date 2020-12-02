95f186d309e6e2c407059438406beb274324ac90
Hieu Nguyen <minhhieun4@gmail.com>
2020-12-02 10:47:18 +0700

    add trend and seasonal

--- model/utils/ed_lstm.py
+++ model/utils/ed_lstm.py
@@ -5,32 +5,53 @@ from keras import backend as K
 import pandas as pd
 
 
-def create_data_prediction_overlap_all(dataset_gsmap, wind_u_mean, wind_v_mean, surface_temp, dataset_gauge, **kwargs):
-    
+def create_data_prediction_overlap_all(dataset_gsmap, wind_u_mean, wind_v_mean,
+                                       surface_temp, precip_seasonal,
+                                       precip_trend, dataset_gauge, **kwargs):
+
     input_dim = kwargs['model'].get('input_dim')
     output_dim = kwargs['model'].get('output_dim')
     seq_len = kwargs['model'].get('seq_len')
     horizon = kwargs['model'].get('horizon')
     T = len(dataset_gsmap)
     col = dataset_gsmap.shape[1]
-    input_encoder = np.zeros(shape=(T*col, seq_len, input_dim))
-    input_decoder = np.zeros(shape=(T*col, seq_len, output_dim))
-    output_decoder = np.zeros(shape=(T*col, seq_len, output_dim))
+    input_encoder = np.zeros(shape=(T * col, seq_len, input_dim))
+    input_decoder = np.zeros(shape=(T * col, seq_len, output_dim))
+    output_decoder = np.zeros(shape=(T * col, seq_len, output_dim))
 
     for col in range(dataset_gsmap.shape[1]):
-        for row in range(T-seq_len-horizon):
-            input_encoder[col*T + row, :, 0] = dataset_gsmap[row+horizon:row+seq_len+horizon, col].copy()
-            input_encoder[col*T + row, :, 1] = wind_u_mean[row+horizon:row+seq_len+horizon, col].copy()
-            input_encoder[col*T + row, :, 2] = wind_v_mean[row+horizon:row+seq_len+horizon, col].copy()
-            input_encoder[col*T + row, :, 3] = surface_temp[row+horizon:row+seq_len+horizon, col].copy()
-            input_decoder[col*T + row, :, 0] = dataset_gauge[row+horizon-1:row+seq_len+horizon-1, col].copy()
-            input_decoder[col*T + row, 0, 0] = 0
-            output_decoder[col*T + row, :, 0] = dataset_gauge[row+horizon:row+seq_len+horizon, col].copy()
+        for row in range(T - seq_len - horizon):
+            input_encoder[col * T + row, :,
+                          0] = dataset_gsmap[row + horizon:row + seq_len +
+                                             horizon, col].copy()
+            input_encoder[col * T + row, :,
+                          1] = wind_u_mean[row + horizon:row + seq_len +
+                                           horizon, col].copy()
+            input_encoder[col * T + row, :,
+                          2] = wind_v_mean[row + horizon:row + seq_len +
+                                           horizon, col].copy()
+            input_encoder[col * T + row, :,
+                          3] = surface_temp[row + horizon:row + seq_len +
+                                            horizon, col].copy()
+            input_encoder[col * T + row, :,
+                          4] = precip_seasonal[row + horizon:row + seq_len +
+                                            horizon, col].copy()
+            input_encoder[col * T + row, :,
+                          5] = precip_trend[row + horizon:row + seq_len +
+                                            horizon, col].copy()
+            input_decoder[col * T + row, :,
+                          0] = dataset_gauge[row + horizon - 1:row + seq_len +
+                                             horizon - 1, col].copy()
+            input_decoder[col * T + row, 0, 0] = 0
+            output_decoder[col * T + row, :,
+                           0] = dataset_gauge[row + horizon:row + seq_len +
+                                              horizon, col].copy()
 
     return input_encoder, input_decoder, output_decoder
 
+
 def create_data_prediction_all(dataset_gsmap, dataset_gauge, **kwargs):
-    
+
     input_dim = kwargs['model'].get('input_dim')
     output_dim = kwargs['model'].get('output_dim')
     seq_len = kwargs['model'].get('seq_len')
@@ -40,11 +61,14 @@ def create_data_prediction_all(dataset_gsmap, dataset_gauge, **kwargs):
     input_decoder = np.zeros(shape=(T, seq_len, input_dim))
     output_decoder = np.zeros(shape=(T, seq_len, output_dim))
 
-    for row in range(T-seq_len-horizon):
-        input_encoder[row, :, :] = dataset_gsmap[row+horizon:row+seq_len+horizon].copy()
-        input_decoder[row, :, :] = dataset_gauge[row+horizon-1:row+seq_len+horizon-1].copy()
+    for row in range(T - seq_len - horizon):
+        input_encoder[row, :, :] = dataset_gsmap[row + horizon:row + seq_len +
+                                                 horizon].copy()
+        input_decoder[row, :, :] = dataset_gauge[row + horizon - 1:row +
+                                                 seq_len + horizon - 1].copy()
         input_decoder[row, 0, :] = 0
-        output_decoder[row, :, :] = dataset_gauge[row+horizon:row+seq_len+horizon].copy()
+        output_decoder[row, :, :] = dataset_gauge[row + horizon:row + seq_len +
+                                                  horizon].copy()
 
     return input_encoder, input_decoder, output_decoder
 
@@ -58,11 +82,15 @@ def load_dataset(**kwargs):
     wind_v_mean = wind_v_mean.reshape([-1, 1])
     surface_temp = pd.read_csv('data/ann/surface_temp.csv').to_numpy()
     surface_temp = surface_temp.reshape([-1, 1])
+    precip_trend = pd.read_csv('data/ann/precip_trend.csv').to_numpy()
+    precip_trend = precip_trend.reshape([-1, 1])
+    precip_seasonal = pd.read_csv('data/ann/precip_seasonal.csv').to_numpy()
+    precip_seasonal = precip_seasonal.reshape([-1, 1])
     dataset_gauge = pd.read_csv('data/ann/gauge.csv').to_numpy()
     dataset_gauge = dataset_gauge.reshape([-1, 1])
     # dataset_gsmap = dataset_gsmap[:, 0]
     # dataset_gauge = dataset_gauge[:, 0]
-    
+
     # dataset_gsmap = np.reshape(dataset_gsmap, (dataset_gsmap.shape[0], 1))
     # dataset_gauge = np.reshape(dataset_gauge, (dataset_gauge.shape[0], 1))
     # dataset_gsmap = np.tile(dataset_gsmap, (1,72))
@@ -74,8 +102,12 @@ def load_dataset(**kwargs):
     wind_u_mean = scaler.transform(wind_u_mean)
     wind_v_mean = scaler.transform(wind_v_mean)
     surface_temp = scaler.transform(surface_temp)
+    precip_seasonal = scaler.transform(precip_seasonal)
+    precip_trend = scaler.transform(precip_trend)
 
-    input_encoder, input_decoder, target_decoder = create_data_prediction_overlap_all(dataset_gsmap, wind_u_mean, wind_v_mean, surface_temp, dataset_gauge, **kwargs)
+    input_encoder, input_decoder, target_decoder = create_data_prediction_overlap_all(
+        dataset_gsmap, wind_u_mean, wind_v_mean, surface_temp, precip_seasonal,
+        precip_trend, dataset_gauge, **kwargs)
     test_size = kwargs['data'].get('test_size')
     valid_size = kwargs['data'].get('valid_size')
 
@@ -86,10 +118,11 @@ def load_dataset(**kwargs):
         input_decoder, test_size=test_size, valid_size=valid_size)
     target_decoder_train, target_decoder_valid, target_decoder_test = common_util.prepare_train_valid_test(
         target_decoder, test_size=test_size, valid_size=valid_size)
-        
+
     data = {}
     for cat in ["train", "valid", "test"]:
-        e_x, e_y, d_y = locals()["input_encoder_" + cat], locals()["input_decoder_" + cat], locals()["target_decoder_" + cat]
+        e_x, e_y, d_y = locals()["input_encoder_" + cat], locals()[
+            "input_decoder_" + cat], locals()["target_decoder_" + cat]
         data["input_encoder_" + cat] = e_x
         data["input_decoder_" + cat] = e_y
         data["target_decoder_" + cat] = d_y
--- test.py
+++ test.py
@@ -1,19 +1,35 @@
 import numpy as np
 
-data_npz = './data/conv2d_gsmap/all_data.npz'
+# data_npz = './data/conv2d_gsmap/all_data.npz'
 
-map_lon = np.load(data_npz)['map_lon']
-map_lat = np.load(data_npz)['map_lat']
-map_precip = np.load(data_npz)['map_precip']
-map_cloud_cover = np.load(data_npz)['map_cloud_cover']
-map_sea_level = np.load(data_npz)['map_sea_level']
-map_surface_temp = np.load(data_npz)['map_surface_temp']
-map_wind_u_mean = np.load(data_npz)['map_wind_u_mean']
-map_wind_v_mean = np.load(data_npz)['map_wind_v_mean']
-gauge_precip = np.load(data_npz)['gauge_precip']
+# map_lon = np.load(data_npz)['map_lon']
+# map_lat = np.load(data_npz)['map_lat']
+# map_precip = np.load(data_npz)['map_precip']
+# map_cloud_cover = np.load(data_npz)['map_cloud_cover']
+# map_sea_level = np.load(data_npz)['map_sea_level']
+# map_surface_temp = np.load(data_npz)['map_surface_temp']
+# map_wind_u_mean = np.load(data_npz)['map_wind_u_mean']
+# map_wind_v_mean = np.load(data_npz)['map_wind_v_mean']
+# gauge_precip = np.load(data_npz)['gauge_precip']
 
-np.savetxt('./data/ann/cloud_cover.csv', map_cloud_cover, delimiter=",")
-np.savetxt('./data/ann/sea_level.csv', map_sea_level, delimiter=",")
-np.savetxt('./data/ann/surface_temp.csv', map_surface_temp, delimiter=",")
-np.savetxt('./data/ann/wind_u_mean.csv', map_wind_u_mean, delimiter=",")
-np.savetxt('./data/ann/wind_v_mean.csv', map_wind_v_mean, delimiter=",")
+# np.savetxt('./data/ann/cloud_cover.csv', map_cloud_cover, delimiter=",")
+# np.savetxt('./data/ann/sea_level.csv', map_sea_level, delimiter=",")
+# np.savetxt('./data/ann/surface_temp.csv', map_surface_temp, delimiter=",")
+# np.savetxt('./data/ann/wind_u_mean.csv', map_wind_u_mean, delimiter=",")
+# np.savetxt('./data/ann/wind_v_mean.csv', map_wind_v_mean, delimiter=",")
+
+import pandas as pd
+from pandas import read_csv
+from matplotlib import pyplot
+import numpy as np
+from statsmodels.tsa.seasonal import seasonal_decompose
+series = read_csv('./data/ann/gsmap.csv', names=range(0, 72))
+series.index = pd.DatetimeIndex(freq='w', start=0, periods=len(series))
+result = seasonal_decompose(series[0])
+trend = result.trend
+seasonal = result.seasonal
+trend = trend.fillna(0)
+print(seasonal.isnull().sum(axis = 0))
+print(trend.isnull().sum(axis = 0))
+trend.to_csv('./data/ann/precip_trend.csv', index=False)
+seasonal.to_csv('./data/ann/precip_seasonal.csv', index=False)
