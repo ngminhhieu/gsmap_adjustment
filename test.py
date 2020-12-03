fb1c27a5df23e162c26251fd0f907fe3fa156b49
Hieu Nguyen <minhhieun4@gmail.com>
2020-12-03 13:38:00 +0700

    fix

--- test.py
+++ test.py
@@ -23,16 +23,45 @@ from pandas import read_csv
 from matplotlib import pyplot
 import numpy as np
 from statsmodels.tsa.seasonal import seasonal_decompose
+from matplotlib import pyplot as plt
+import random
+import scipy.stats
 
-series = read_csv('./data/ann/gsmap.csv', names=range(0, 72))
-trend = np.empty(shape=(len(series), len(series.columns)))
-seasonal = np.empty(shape=(len(series), len(series.columns)))
-print(trend.shape)
-series.index = pd.DatetimeIndex(freq='w', start=0, periods=len(series))
-for i in range(len(series.columns)):
-    result = seasonal_decompose(series[i])
-    trend[:, i] = result.trend.fillna(0)
-    seasonal[:, i] = result.seasonal
+# series = read_csv('./data/ann/gsmap.csv', names=range(0, 72))
+# trend = np.empty(shape=(len(series), len(series.columns)))
+# seasonal = np.empty(shape=(len(series), len(series.columns)))
+# print(trend.shape)
+# series.index = pd.DatetimeIndex(freq='w', start=0, periods=len(series))
+# for i in range(len(series.columns)):
+#     result = seasonal_decompose(series[i])
+#     trend[:, i] = result.trend.fillna(0)
+#     seasonal[:, i] = result.seasonal
 
-np.savetxt('./data/ann/precip_seasonal.csv', trend, delimiter=',')
-np.savetxt('./data/ann/precip_trend.csv', trend, delimiter=',')
+# np.savetxt('./data/ann/precip_seasonal.csv', trend, delimiter=',')
+# np.savetxt('./data/ann/precip_trend.csv', trend, delimiter=',')
+
+# data = np.empty(shape=(200, 3))
+# for i in range(len(data)):
+#     data[i, 0] = i
+#     data[i, 1] = i%4
+
+# for ran in range(50):
+#     ran = random.randint(0, 10)
+#     data[ran, 0] = ran
+
+# data[0:20, 2] = data[0:20, 1]
+# data[20:100, 2] = data[20:100, 1] + 2
+# data[100:200, 2] = data[100:200, 1] -3
+# np.savetxt('./data/ann/test.csv', data, delimiter=',')
+
+# for i in range(3):
+#     plt.plot(data[:, i])
+#     plt.show()
+
+
+x = np.arange(5.)
+y = np.arange(5.)
+print(scipy.stats.pearsonr(x, y))
+
+plt.scatter(x, y)
+plt.show()
